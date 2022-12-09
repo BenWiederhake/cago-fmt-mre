128bd3bef4f6b1ddbeee62b3e83452194d4e7fc0
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2022-12-09 14:40:18 +0100

    Replaces names and values

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
-fn run_test(instruction_prefix: &[u16], max_steps: usize) {
-    println!(
-        "Got: {:?}, {:?}",
-        instruction_prefix, max_steps
-    );
+fn some_func(foo: &[u16], bar: usize) {
+    println!("Got: {:?}, {:?}", foo, bar);
 }
 
 fn main() {
-    run_test(
+    some_func(
         &[
-                    // asdf
-            0x31,   // ↓
-            0x4100, // lw r1, a
-            0x32,   // ↓
-                    // asdf
-            0x4200, // lw r2, b
-            0x60,   // binary.function r2, r1
-                    // asdf
+                    // Let's say I have a bunch of numbers.
+            0,      // For example, a short 0. But lots of comments are needed.
+            0x1234, // Or a slightly longer value.
+            0xFF,   // Fun fact: My personal use-case is providing hand-written
+                    // assembly, and not all directives translate to
+                    // instructions (e.g. labels).
+            0x666,  // Wouldn't it be nice if all comments were aligned?
+            0x42,   // However, cargo-fmt strongly disagrees with that.
+                    // Hence, this minimal reproducing example.
         ],
-        5,
+        5, // Necessary, for some reason.
     );
 }
